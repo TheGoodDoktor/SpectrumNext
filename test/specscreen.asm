390c108445ee1b73a6cf4bb6e7a423d72452204d
lucidgames <support@lucidgames.co.uk>
2017-06-16 16:10:23 +0100

    map screen drawing

--- test/specscreen.asm
+++ test/specscreen.asm
@@ -133,8 +133,13 @@ bigtile_data_ptr 	dw 0
 tile_data_ptr 		dw 0
 tile_image_data_ptr dw 0
 
+draw_tile_xy dw 0
+
 ; Draw a small tile at (B,C)
 ; A is tile number
+draw_small_tile_xy:
+	ld bc,(draw_tile_xy)
+	
 draw_small_tile:
 	; calculate tile image addr
 	ld ix, (tile_data_ptr)	; make ix point to tile data ; todo: indirection?
@@ -192,11 +197,130 @@ draw_tile_char 3,1,1
 
 ret
 
-; draw a big tile at BC
-; a big tile index
+; draw a big tile 
+; A: big tile index
+draw_big_tile_xy dw 0
+
 draw_big_tile:
 
+	; store coords in memory
+	ld bc, (draw_big_tile_xy)
+	ld (draw_tile_xy),bc
+		
+	ld ix, (bigtile_data_ptr)
+	
+	; 12 bytes per big tile so shift by 8 & 4 and add
+	; add a * 8 
+	ld d,0
+	ld e,a
+	rl e
+	rl d
+	rl e
+	rl d
+	rl e
+	rl d
+	add ix,de	; ix points to char def
+	; add a *4
+	ld d,0
+	ld e,a
+	rl e
+	rl d
+	rl e
+	rl d
+	add ix,de	; ix points to char def
 
+	ld b,3
+	bt_yloop:	; vertical tile loop
+	
+		push bc	; push y counter
+		ld b,4
+		bt_xloop:	; horizontal tile loop
+		
+			ld a,(ix)	; load tile number
+			
+			push ix
+			push bc
+			call draw_small_tile_xy
+			pop bc
+			pop ix
+			
+			inc ix
+			
+			ld a,(draw_tile_xy)	; add 2 to x coord
+			inc a
+			inc a
+			ld (draw_tile_xy),a
+			
+			djnz bt_xloop
+			
+		ld a,(draw_tile_xy)	; reset x coord
+		sub 8
+		ld (draw_tile_xy),a
+		ld a,(draw_tile_xy+1)
+		inc a
+		inc a
+		ld (draw_tile_xy+1),a
+				
+		pop bc ; pop y counter
+	djnz bt_yloop
+
+ret
+
+; draw a screen 
+; a contains screen number
+draw_screen:
+	ld bc,0
+	ld (draw_big_tile_xy),bc
+	
+	ld ix, (screen_data_ptr)
+	
+	; 16 bytes per screen
+	; add a * 16 
+	ld d,0
+	ld e,a
+	rl e
+	rl d
+	rl e
+	rl d
+	rl e
+	rl d
+	rl e
+	rl d
+	add ix,de	; ix points to char def
+	
+	ld b,4
+	scr_yloop:	; vertical big tile loop
+	
+		push bc	; push y counter
+		ld b,4
+		
+		scr_xloop:	; horizontal big tile loop
+		
+			ld a,(ix)	; load big tile number
+			push ix
+			push bc
+			call draw_big_tile
+			pop bc
+			pop ix
+			inc ix
+			
+			ld a,(draw_big_tile_xy)	; add 8 to x coord
+			add a,8	
+			ld (draw_big_tile_xy),a
+			
+			djnz scr_xloop
+		
+		; reset x		
+		xor a
+		ld (draw_big_tile_xy),a
+		
+		; increase y
+		ld a,(draw_big_tile_xy+1)
+		add a,6
+		ld (draw_big_tile_xy+1),a
+				
+		pop bc ; pop y counter
+	djnz scr_yloop
 ret
 
 ; >>>> TODO: move to a new file
@@ -223,18 +347,19 @@ ld b,10
 ld c,10
 ;ld hl, tile_char_images
 ;call spec_screen_draw_char
-ld a,1
-call draw_small_tile
-
+ld a,0
+call draw_screen
+;ld a,1
+;call draw_small_tile_xy
 ret
 
 ; big tile data
 ; 4x3
-big_tile_data:
+bigtile_data:
 	; big tile 0
-	db 0,0,0,0
-	db 1,1,1,1
-	db 0,1,0,1
+	db 1,0,0,1
+	db 0,1,1,0
+	db 1,0,0,1
 	
 ; screen data
 ; 4 * 4 big tiles
@@ -253,7 +378,7 @@ tile_data:
 	db $38,$38,$38,$38	; black on white
 	; tile 1
 	db 1,2,2,1	; solid tile
-	db $38,$04,$38,$0f	; black on white
+	db $38,$04,$38,$0f	; random
 
 ; bitmap data of tile characters
 tile_char_images:
